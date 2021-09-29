56b1bac2433e1fc3d32e4ca6dfcb374b6d58aa93
unknown <Admin@proger-.01>
2021-09-29 17:22:07 +0600

    add contains

--- src/beks/Main.java
+++ src/beks/Main.java
@@ -3,6 +3,13 @@ package beks;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Bke");
+        int a = 5;
+        a += 10;        // 15
+        a -= 3;         // 12
+        a *= 2;         // 24
+        a /= 6;         // 4
+        a <<= 4;      // 64
+        a >>= 2;      // 16
+        System.out.println(a);  // 16
     }
 }
